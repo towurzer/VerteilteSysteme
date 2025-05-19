ef8e1e8d745a0675a5959ca388a7089c996b6eda
Philipp Wolfger <philipp.wolfger@alturos.com>
2025-05-19 11:21:06 +0200

    Added structure for assignment 9

---
+++ src/main/java/org/aau/homework/assignment_09/ex1/Main.java
@@ -0,0 +1,7 @@
+package org.aau.homework.assignment_09.ex1;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/main/java/org/aau/homework/assignment_09/ex2/Main.java
@@ -0,0 +1,7 @@
+package org.aau.homework.assignment_09.ex2;
+
+public class Main {
+    public static void main(String[] args) {
+        
+    }
+}
